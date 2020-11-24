2eebf8355f42fde631c538e404fda74f4bec1286
Mike Timberg <timberg.1@osu.edu>
2020-11-24 17:46:03 -0500

    cleanup

--- error_percentage/error_percentage.py
+++ error_percentage/error_percentage.py
@@ -14,28 +14,19 @@ def error_percentage(file_path):
     Hopefully, when complete, this script will:
      1. Read input file in chunks
      2. Read chunks line by line
-     3.
+     3. TODO
     :param file_path:
     :return: Boolean if any exceptions were raised.  For exit codes.
     :raise OSError:  if the file path is invalid or if the file cannot be read.
     """
-    # pass
-    # print('1')
-    # click.echo(message='3')
-    # click.echo()
     try:
         with open(buffering=1, file=file_path,
                   mode='rt', encoding='utf8', ) as file:
-            # print('2')
-            # click.echo(message='4')
-            # [print(line) for line in file]
             [print(line, end='') for line in file]
-            # list(map(lambda line: print(line), file))
     except Exception as error:
         exception(msg=error)
         return False
 
 
 if __name__ == '__main__':
-    # pass
     exit(0 if bool(error_percentage()) else 1)
--- test/test_error_percentage.py
+++ test/test_error_percentage.py
@@ -1,20 +1,10 @@
 from click.testing import CliRunner
 from error_percentage.error_percentage import error_percentage
-# import error_percentage
 
 
-# def test():
 def test_reading_file():
-    # error_percentage(file_path='../test/log_file.py')
     result = CliRunner().invoke(error_percentage, ['--file-path',
                                                    './test/log_sample.txt'])
-    # print(dir(something))
-    # print(something.exit_code)
-    # print(something.output)
 
-    # print(result.output)
-    # print(result.output.splitlines().pop(0).split(' | ').pop(0))
-    # assert False
     assert result.output.splitlines().pop(0) \
         .split(' | ').pop(0) == '1493969101.638'
-    # assert True
