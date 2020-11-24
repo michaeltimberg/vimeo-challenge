4295fff9490e06f33f7aeefe47df7956fa765bc2
Mike Timberg <timberg.1@osu.edu>
2020-11-24 17:44:56 -0500

    reads file!

--- error_percentage/error_percentage.py
+++ error_percentage/error_percentage.py
@@ -1,14 +1,41 @@
+from logging import exception
+
 import click
 
 
-def error_percentage():
+@click.command()
+@click.option('--file-path',
+              '-f',
+              help='Relative path to log file.',
+              required=True,
+              type=str)
+def error_percentage(file_path):
     """
-    None
-    :param: None
-    :return: None
+    Hopefully, when complete, this script will:
+     1. Read input file in chunks
+     2. Read chunks line by line
+     3.
+    :param file_path:
+    :return: Boolean if any exceptions were raised.  For exit codes.
+    :raise OSError:  if the file path is invalid or if the file cannot be read.
     """
-    pass
+    # pass
+    # print('1')
+    # click.echo(message='3')
+    # click.echo()
+    try:
+        with open(buffering=1, file=file_path,
+                  mode='rt', encoding='utf8', ) as file:
+            # print('2')
+            # click.echo(message='4')
+            # [print(line) for line in file]
+            [print(line, end='') for line in file]
+            # list(map(lambda line: print(line), file))
+    except Exception as error:
+        exception(msg=error)
+        return False
 
 
 if __name__ == '__main__':
-    pass
+    # pass
+    exit(0 if bool(error_percentage()) else 1)
